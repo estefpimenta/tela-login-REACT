7bde8339ca8fd9e7189aa383c7da4c7d26439a53
Estefanno <estefanno.pimenta@gmail.com>
2025-11-25 15:14:33 -0300

    updates

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,22 +1,36 @@
 import {FaUser, FaLock} from 'react-icons/fa';
+import { useState } from 'react';
 import './Login.css'
 
 const Login = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Envio")
+    }
+
+
+
   return (
 
     <div className="container">
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
 
             <h1>Acesse o sistema</h1>
 
             <div>
-                <input type="email" placeholder='E-mail' />
+                <input type="email" placeholder='E-mail' 
+                onChange={(e) => setUsername(e.target.value)}/>
                 <FaUser className='icon' />
             </div>
                 
             <div>
-                <input type="password" placeholder='Senha' />
+                <input type="password" placeholder='Senha' 
+                onChange={(e) => setPassword(e.target.value)}/>
                 <FaLock className='icon' />
             </div>
                    
