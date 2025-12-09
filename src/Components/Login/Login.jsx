943cb8702586ff18d4e81e41dbf88abaa4e03d4f
Estefanno <estefanno.pimenta@gmail.com>
2025-12-09 15:46:39 -0300

    css styles add

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -22,13 +22,13 @@ const Login = () => {
 
             <h1>Acesse o sistema</h1>
 
-            <div>
-                <input type="email" placeholder='E-mail' 
+            <div className='input-field'>
+                <input type="email" placeholder='E-mail' required
                 onChange={(e) => setUsername(e.target.value)}/>
                 <FaUser className='icon' />
             </div>
                 
-            <div>
+            <div className='input-field'>
                 <input type="password" placeholder='Senha' 
                 onChange={(e) => setPassword(e.target.value)}/>
                 <FaLock className='icon' />
@@ -36,13 +36,12 @@ const Login = () => {
                    
             <div className="recall-forget">
                 <label>
-                    <input type="checkbox" />
-                    Lembre de mim
-                    <a href="#">Esqueceu a senha?</a>
+                     Lembre de mim <input type="checkbox"/>
                 </label>
+                <a href="#">Esqueceu a senha?</a>
             </div>
 
-            <button>Entrar</button>
+            <button>Entrar 🤙</button>
 
             <div className="signup-link">
                 <p>Não tem uma coonta? <a href="#">Registrar</a></p>
